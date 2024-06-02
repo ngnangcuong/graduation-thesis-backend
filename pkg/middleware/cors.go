4c2596f99841df6aec72138a52b403e4f8138bdd
ngnangcuong <nangcuonga1k45@gmail.com>
2024-06-02 08:49:20 +0700

    fix: add X-User-ID in allow headers

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -8,7 +8,7 @@ func SetupCors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header, Access-Control-Allow-Origin")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header, Access-Control-Allow-Origin, X-User-ID")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
