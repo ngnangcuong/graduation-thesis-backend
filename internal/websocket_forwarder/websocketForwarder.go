88aba81b7aaa134034637645e997f342a2a1e185
ngnangcuong <nangcuonga1k45@gmail.com>
2024-06-20 19:21:15 +0700

    update: switch http to https for websocket forwarder

--- internal/websocket_forwarder/websocketForwarder.go
+++ internal/websocket_forwarder/websocketForwarder.go
@@ -1,6 +1,7 @@
 package websocket_forwarder
 
 import (
+	"crypto/tls"
 	"fmt"
 	"graduation-thesis/internal/websocket_forwarder/handler"
 	"graduation-thesis/pkg/custom_error"
@@ -42,12 +43,26 @@ func Run() {
 	)
 	router := handler.GetRouter(websocketForwarder)
 
+	TLSConfig := &tls.Config{
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+		},
+		MinVersion:         tls.VersionTLS12,
+		InsecureSkipVerify: true,
+	}
 	srv := http.Server{
 		Addr:         fmt.Sprintf(":%d", viper.GetInt("app.port")),
 		ReadTimeout:  5 * time.Minute,
 		WriteTimeout: 10 * time.Minute,
 		IdleTimeout:  1 * time.Minute,
 		Handler:      router,
+		TLSConfig:    TLSConfig,
 	}
 
 	var wg sync.WaitGroup
@@ -56,7 +71,7 @@ func Run() {
 
 	go func(wg *sync.WaitGroup) {
 		defer wg.Done()
-		if err := srv.ListenAndServe(); err != nil {
+		if err := srv.ListenAndServeTLS(viper.GetString("app.cert"), viper.GetString("app.key")); err != nil {
 			panic(err)
 		}
 	}(&wg)
