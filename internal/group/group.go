5766b57c012bd9c0318462aa79339e23b0cc7564
ngnangcuong <nangcuonga1k45@gmail.com>
2024-06-20 19:24:38 +0700

    update: add https serve into group service

--- internal/group/group.go
+++ internal/group/group.go
@@ -1,6 +1,7 @@
 package group
 
 import (
+	"crypto/tls"
 	"fmt"
 	"graduation-thesis/internal/group/handler"
 	"graduation-thesis/internal/group/repository"
@@ -42,8 +43,30 @@ func Run() {
 
 	router := handler.GetRouter(groupHandler, conversationHandler)
 
-	srv := http.Server{
-		Addr:         fmt.Sprintf(":%d", viper.GetInt("app.port")),
+	TLSConfig := &tls.Config{
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+		},
+		MinVersion:         tls.VersionTLS12,
+		InsecureSkipVerify: true,
+	}
+	httpsSrv := http.Server{
+		Addr:         fmt.Sprintf(":%d", viper.GetInt("app.https_port")),
+		ReadTimeout:  5 * time.Minute,
+		WriteTimeout: 10 * time.Minute,
+		IdleTimeout:  1 * time.Minute,
+		Handler:      router,
+		TLSConfig:    TLSConfig,
+	}
+
+	httpSrv := http.Server{
+		Addr:         fmt.Sprintf(":%d", viper.GetInt("app.http_port")),
 		ReadTimeout:  5 * time.Minute,
 		WriteTimeout: 10 * time.Minute,
 		IdleTimeout:  1 * time.Minute,
@@ -51,11 +74,17 @@ func Run() {
 	}
 
 	var wg sync.WaitGroup
-	wg.Add(1)
+	wg.Add(2)
 
 	go func(wg *sync.WaitGroup) {
 		defer wg.Done()
-		if err := srv.ListenAndServe(); err != nil {
+		if err := httpsSrv.ListenAndServeTLS(viper.GetString("app.cert"), viper.GetString("app.key")); err != nil {
+			panic(err)
+		}
+	}(&wg)
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		if err := httpSrv.ListenAndServe(); err != nil {
 			panic(err)
 		}
 	}(&wg)
